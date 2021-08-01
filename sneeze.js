99adb708cfca4fd40f18b6d44754e401a240434a
cxxerry <78811739+cxxerry@users.noreply.github.com>
2021-08-01 21:36:49 +0900

    Add files via upload

---
+++ sneeze.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  var d = new Date();
+  var n = d.getHours();
+  if(n > 21 || n < 6){
+    document.querySelector('body').style.backgroundColor = '#232323';
+    document.querySelector('body').style.color = 'white';
+    var alist = document.querySelectorAll('a');
+    var i = 0;
+    while (i < alist.length) {
+      alist[i].style.color = 'gray';
+      i = i + 1;
+    }
+  }
+  else{
+    document.querySelector('body').style.backgroundColor = 'white';
+    document.querySelector('body').style.color = 'black';
+    var alist = document.querySelectorAll('a');
+    var i = 0;
+    while (i < alist.length) {
+      alist[i].style.color = 'black';
+      i = i + 1;
+    }
+  }
+});
